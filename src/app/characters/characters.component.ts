2c159f3891451f84194c74375691f5b86689fcad
Jaider <jaidercueto.1010@gmail.com>
2026-01-17 14:00:56 -0500

    Inyectando servicio 'RickMortyService' en el componente CharactersComponent

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickMortyService } from '../core/services/rick-morty.service';
+import { Character } from '../core/models/character.model';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.scss'
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit {
+  characters: Character[] = [];
+  loading: boolean = false;
 
+  constructor(private rickMortyService: RickMortyService) {  }
+
+  ngOnInit(): void {
+    this.loadCharacters();
+  }
+
+  loadCharacters(): void {
+    this.loading = true;
+    this.rickMortyService.getCharacters().subscribe({
+      next: (response) => {
+        this.characters = response.results;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      }
+    });
+  }
 }
