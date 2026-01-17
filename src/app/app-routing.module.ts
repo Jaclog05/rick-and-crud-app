77b62d1212878eec040f80c47323a44954a3ee02
Jaider <jaidercueto.1010@gmail.com>
2026-01-17 13:13:35 -0500

    Renderizando el FavoritesComponent en la ruta /favorites.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharactersComponent } from './characters/characters.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 const routes: Routes = [
   { path: 'characters', component: CharactersComponent },
+  { path: 'favorites', component: FavoritesComponent },
   { path: '', redirectTo: '/characters', pathMatch: 'full' }
 ];
 
