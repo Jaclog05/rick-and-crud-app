5ee355b0b67ffaca55f21e5e3556b7455c87f3d9
Jaider <jaidercueto.1010@gmail.com>
2026-01-17 12:51:04 -0500

    Creando componente characters dentro de modulo characters

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CharactersRoutingModule } from './characters-routing.module';
+import { CharactersComponent } from './characters.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CharactersComponent
+  ],
   imports: [
     CommonModule,
     CharactersRoutingModule
