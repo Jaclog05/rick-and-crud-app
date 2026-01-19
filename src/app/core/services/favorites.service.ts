d61548156d2726aac9b5161b4ddcfbcb748c0646
Jaider <jaidercueto.1010@gmail.com>
2026-01-18 22:25:18 -0500

    Creando el método 'addFavorite' en FavoriteService

--- src/app/core/services/favorites.service.ts
+++ src/app/core/services/favorites.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { FavoriteCharacter } from '../models/character.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
+  private favoriteSubject = new BehaviorSubject<FavoriteCharacter[]>([])
+
+  favorites$: Observable<FavoriteCharacter[]> = this.favoriteSubject.asObservable()
 
   constructor() { }
+
+  get currentFavorites(): FavoriteCharacter[] {
+    return this.favoriteSubject.value;
+  }
+
+  addFavorite(newFavorite: FavoriteCharacter): void {
+    const current = this.currentFavorites
+
+    if(!current.find(character => character.id === newFavorite.id)) {
+      const updated = [...current, newFavorite]
+      this.favoriteSubject.next(updated)
+    }
+  }
 }
